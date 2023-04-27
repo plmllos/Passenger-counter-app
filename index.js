10323e49818b6f40702a257d8af988f19a9afbcd
Milos Popovic <popovic.milos@live.com>
2023-04-27 13:46:32 +0200

    Refactored JS code with new syntax and functions learned

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 let count = 0
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+const countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
 
-let decrementBtn = document.getElementById("decrement-btn")
-let saveBtn = document.getElementById("save-btn")
+const incrementBtn = document.getElementById("increment-btn")
+const decrementBtn = document.getElementById("decrement-btn")
+const saveBtn = document.getElementById("save-btn")
+const resetBtn = document.getElementById("reset-btn")
+const allBtns = document.getElementsByClassName('btn')
 
-decrementBtn.disabled = true
-saveBtn.disabled = true
+incrementBtn.addEventListener('click', render)
+decrementBtn.addEventListener('click', render)
 
-function increment() {
-    decrementBtn.disabled = false
+function render(e) {
     saveBtn.disabled = false
-    count ++
-    countEl.textContent = count 
-}
-
-function decrement() {
-    if (count > 1) {
+    resetBtn.disabled = false
+    let checkId = e.target.id
+    if (checkId === "increment-btn") {
+        count ++
+        for (let btn of allBtns) {
+            btn.classList.add('active')
+        }
+    } else if (checkId === "decrement-btn" && count > 0) {
         count --
-        countEl.textContent = count
-    } else {
-        alert("Zero or negative numbers are not valid for the entry")
     }
+    countEl.innerHTML = count
 }
 
-function save() {
-    let countString = count + ' - '
-    saveEl.textContent += countString
+saveBtn.addEventListener('click', function() {
+    if (count > 0) {
+        let countString = count + '- '
+        saveEl.innerHTML +=  `
+        <span> ${countString} </span>
+    `
+    }
+    for (let btn of allBtns) {
+        btn.classList.remove('active')
+    }
     count = 0
     countEl.innerText = 0
-    decrementBtn.disabled = true
-    saveBtn.disabled = true
-}
+})
 
-function reset() {
+resetBtn.addEventListener('click', function() {
     count = 0
-    countEl.textContent = 0
-    saveEl.textContent = ""
-    decrementBtn.disabled = true
-    saveBtn.disabled = true
-}
+    countEl.innerText = 0
+    saveEl.textContent = 'Previous entries: '
+    for (let btn of allBtns) {
+        btn.classList.remove('active')
+    }
+    
+})
